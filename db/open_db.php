ffdde95cc5e4f635a20a5bdd976529bbcaca1522
cdavidmr <cdavidmr@outlook.com>
2018-10-01 15:40:58 -0500

    Update project with streaming module

--- db/open_db.php
+++ db/open_db.php
@@ -4,13 +4,14 @@ $usuariobd="root";
 $passwordbd="";
 $basededatos="learn_teaching";
 //lista de tablas
-$tabla1="re_user";
-$tabla2="pro_user";
+$tabla1="registeruser";
+$tabla2="country";
+$tabla3="lang";
 /////
 //No muestre el error
-error_reporting(0);
+//error_reporting(0);
 $conxn = new mysqli($host, $usuariobd, $passwordbd, $basededatos);
-if ($conexion->connect_errno) {
+if ($conxn->connect_errno) {
 	# code...E
 	echo "<div class=\"alert alert-warning\"><strong>Error!</strong> El sitio presenta fallos con la BD.</div>";
 	exit();
